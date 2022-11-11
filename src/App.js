00f2d0db5f947705da3c0cbe83b4fdb3181aa48d
Maria <alece.1414@gmail.com>
2022-11-11 15:02:03 +0300

    project initialization

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+import './style/normalize.css';
+
+
+function App() {
+  return (
+    <Header as="h1">
+      Hello world!
+    </Header>
+  );
+}
+
+export default App;
